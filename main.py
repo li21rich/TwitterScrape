2fbc99667bf70e3bad2491e5261e49e7a827e6e0
li21rich <86533810+li21rich@users.noreply.github.com>
2023-12-25 03:08:34 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,8 @@ username = input("Enter bot twitter username: ")
 password = input("Enter bot twitter password: ")
 headless = input("Enable headless? Enter \"no\" if unsure or see the README.md file: ")
 login_method = input("Enter \"username\" to log in with username or enter \"email\" to log in with email")
-
 minimum = input("Enter minimum number of tweets to scrape: ")
-query = input("Enter search URL to scrape (i.e. https://twitter.com/search?q=Search%20Query%20OR%20SearchQuery&src=typed_query): ")
-
+query = input("Enter search URL to scrape ( i.e. https://twitter.com/search?q=Search%20Query%20OR%20SearchQuery&src=typed_query ): ")
 try:
     bot = tb.Twitterbot(email, password, username, headless)
     if login_method.lower() == "username":
