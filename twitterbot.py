f913275e137fc84cc6f70e62fa8e6b80b404aee3
li21rich <86533810+li21rich@users.noreply.github.com>
2024-06-17 23:02:36 -0700

    Update twitterbot.py
    added scrape_full_page optional function

--- twitterbot.py
+++ twitterbot.py
@@ -58,3 +58,48 @@ class Twitterbot:
 				results += result + "\n"
 		with open("output.html", "w", encoding="utf-8") as file: file.write(results)
 		return results
+
+	#instead of scraping chosen # of tweets, this version thoroughly scrapes the entire page or until a selected limit is reached. 
+	def scrape_full_page(self, query, limit): #If you dont want it to be limited, set limit to an arbitrarily large number.
+		bot = self.bot
+		count = 0
+		i = 0
+		results = ""
+		bot.get(query)
+		print("Scanning", query, "until", limit, "tweets")
+		while True:
+			try:
+				if i > int(limit):
+					print("page done")
+					break
+				WebDriverWait(bot, 10).until(ec.presence_of_element_located(('xpath', "//article[@data-testid='tweet']")))
+				ogheight = bot.execute_script("return document.body.scrollHeight")
+				bot.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+				try:
+					WebDriverWait(bot, 10).until(ec.presence_of_element_located(('xpath', "//article[@data-testid='tweet']")))
+				except:
+					time.sleep(1)
+				if bot.execute_script("return document.body.scrollHeight") == ogheight:
+					time.sleep(1)
+					bot.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+					if bot.execute_script("return document.body.scrollHeight") == ogheight:
+						print("page done")
+						break
+				if i < 1:
+					bot.execute_script('window.scrollTo(0, 0)')
+				elements = WebDriverWait(bot, 10).until(ec.presence_of_all_elements_located(('xpath', "//article[@data-testid='tweet']")))
+				for elem in elements:
+					text = str(elem.find_element("xpath", ".//div[@data-testid='tweetText']").text)
+					text = text.replace("\n\n", "\n").replace("\n\n", "\n")
+					analytic = str(elem.find_element("xpath", ".//div[@role='group']").get_attribute('aria-label'))
+					date = str(elem.find_element("xpath", ".//time").get_attribute('datetime'))
+					date = date.split('T')[0]
+					i += 1
+					count += 1
+					result = str(":::> #" + str(count) + ". " + analytic + " " + date + " <:::\n" + text)
+					print(result)
+					results += result + "\n"
+			except Exception as e:
+				print("page done or error encountered")
+				break
+		return results
