6e9c623cc8144d2f76509d5b6b8a56601ad2cd81
li21rich <86533810+li21rich@users.noreply.github.com>
2024-06-18 20:27:12 -0700

    Update main.py
    simplified log in process (removed login with email)

--- main.py
+++ main.py
@@ -4,15 +4,11 @@ email = input("Enter bot twitter email: ")
 username = input("Enter bot twitter username: ")
 password = input("Enter bot twitter password: ")
 headless = "no";
-login_method = "username" #alternative: login_method = "email"
 minimum = input("Enter minimum number of tweets to scrape: ")
 query = input("Enter search URL to scrape ( i.e. https://twitter.com/search?q=Search%20Query%20OR%20SearchQuery&src=typed_query ): ")
 try:
     bot = tb.Twitterbot(email, password, username, headless)
-    if login_method.lower() == "username":
-        bot.loginWithUsername()
-    else:
-        bot.loginWithEmail
+    bot.loginWithUsername()
 except Exception as e:
     print("Invalid input. Please double check account credentials and ensure that you have inputted valid parameters")
 
